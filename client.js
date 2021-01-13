2579b7af3868ddee9d1e6f4cd309bf81c4ab63bd
gforsythe <gabe.forsythe@gmail.com>
2021-01-13 21:14:50 +0000

    finally got it to work!

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function(){
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+
+    
+  });
+  
+  conn.on('data', (data) => {
+    console.log(data, "its got a sharp tongue this server")
+  })
+
+  conn.setEncoding('utf8');
+  
+  
+  return conn;
+  
+
+}
+
+module.exports = connect 
